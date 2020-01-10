0155d403f481acaed84a6c739bd19a633239cc20
TasfiaAddrita <tasfia.addrita@gmail.com>
2020-01-10 13:36:21 -0800

    setting up heroku

--- server.js
+++ server.js
@@ -22,5 +22,15 @@ mongoose
 // Use Routes
 app.use("/api/grocery-items", groceryItems);
 
+// Serve static assets if in production
+if (process.env.NODE_ENV == "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
