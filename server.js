c02591e1be6a0f26551dedc5b09a9b68f2f1423e
TasfiaAddrita <tasfia.addrita@gmail.com>
2020-04-30 20:55:46 -0700

    added tests

--- server.js
+++ server.js
@@ -36,3 +36,5 @@ if (process.env.NODE_ENV === "production") {
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
+
+module.exports = app
